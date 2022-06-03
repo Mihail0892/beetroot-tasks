62975633d699bc6295e1bde5628971467ff441bf
Mihail0892 <yanamishatereshchenko@gmail.com>
2022-06-03 18:52:48 +0300

    lesson-16

--- lesson-15/index.js
+++ lesson-15/index.js
@@ -5,48 +5,30 @@
     let такМожноНоНенужно = 'vot tak';
 */
 
-let myName = prompt('Звать то как?');
-console.log('Привет,', myName);
+let myName = prompt("Звать то как?");
+console.log("Привет,", myName);
 
-
-let yourBirthYear = prompt('В каком году Вы родились?');
+let yourBirthYear = prompt("В каком году Вы родились?");
 const thisYear = 2022;
 let yourAge = thisYear - yourBirthYear;
-console.log('Ваш возраст:', yourAge );
+console.log("Ваш возраст:", yourAge);
 
-
-let squareSide = prompt('Сторона квадрата равна:');
-squareSide=Number(squareSide);
+let squareSide = prompt("Сторона квадрата равна:");
+squareSide = Number(squareSide);
 let squarePerimeter = squareSide + squareSide + squareSide + squareSide;
-console.log('Периметр квадрата равен:',squarePerimeter);
+console.log("Периметр квадрата равен:", squarePerimeter);
 
-
-let radius = prompt('Чему равен радиус круга?');
+let radius = prompt("Чему равен радиус круга?");
 const Pi = 3.14;
-let cercleArea = Pi*radius*radius;
-console.log('Площадь окружности:',cercleArea );
+let cercleArea = Pi * radius * radius;
+console.log("Площадь окружности:", cercleArea);
 
-
-let distance = prompt('Какое расстояние необходимо проехать?');
-let time = prompt('За какое время хотите добраться?');
+let distance = prompt("Какое расстояние необходимо проехать?");
+let time = prompt("За какое время хотите добраться?");
 let speed = distance / time;
-console.log('Вам необходимо двигаться со скоростью:',speed,'км/ч');
+console.log("Вам необходимо двигаться со скоростью:", speed, "км/ч");
 
-
-let dollars = prompt('Сколько вы хотите обменять?');
+let dollars = prompt("Сколько вы хотите обменять?");
 const ratio = 0.95;
 let euros = dollars * ratio;
-console.log('Сколько вы получите:',euros);
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log("Сколько вы получите:", euros);
